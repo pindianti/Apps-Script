a8b03eafa759ce9a3d9ac8ca227f2bc4ebf3b529
Mila Pindianti <110595502+pindianti@users.noreply.github.com>
2022-09-26 18:55:26 +0700

    Update import.js

--- import.js
+++ import.js
@@ -5,7 +5,7 @@ function Overwrite() {
   
   var uidbackup = sheettobackup.getRange(2,1,maxrow,3).getValues();
   
-  var backupfile = SpreadsheetApp.openById("1c38asYcuVcChbqoVc6dLNvtSCTCI5s9RSiTAMDKQme4");
+  var backupfile = SpreadsheetApp.openById("masukan url");
   var sheettarget = backupfile.getSheetByName("coba");
   //var maxrow2 = sheettarget.getMaxRows();
   //var maxcolumn2 = sheettarget.getMaxColumns();
