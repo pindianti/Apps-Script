5fd6957085ac08b5a3b7f54a0f8fd5cf654f7eb5
Mila Pindianti <110595502+pindianti@users.noreply.github.com>
2022-10-29 10:54:42 +0700

    Update import.js

--- import.js
+++ import.js
@@ -1,14 +1,14 @@
-function Overwrite() {
-  var sheettobackup = SpreadsheetApp.getActive().getSheetByName("data");
-  var maxrow = sheettobackup.getMaxRows();
-  var maxcolumn = sheettobackup.getMaxColumns();
+function ShouDesuka() {
+  var data = SpreadsheetApp.getActive().getSheetByName("M");
+  var maxrow = data.getMaxRows();
+  var maxcolumn = data.getMaxColumns();
+  var range = data.getRange(2,3,maxrow,2).getValues();
+
+  var tujuan = SpreadsheetApp.openById("1c38asYcuVcChbqoVc6dLNvtSCTCI5s9RSiTAMDKQme4"); //sheet tujuan yg pertama
+  var sheet = tujuan.getSheetByName("melmel");
+  sheet.getRange(2,5,maxrow,2).setValues(range);
   
-  var uidbackup = sheettobackup.getRange(2,1,maxrow,3).getValues();
-  
-  var backupfile = SpreadsheetApp.openById("masukan url");
-  var sheettarget = backupfile.getSheetByName("coba");
-  //var maxrow2 = sheettarget.getMaxRows();
-  //var maxcolumn2 = sheettarget.getMaxColumns();
-  
-  sheettarget.getRange(2,1,maxrow,3).setValues(uidbackup);
+  var tujuandua = SpreadsheetApp.openById("1809KdM3gyLXo5loShA2msdl4rCzizW8fNJ-hmBg9C6w"); //sheet tujuan kedua
+  var sheetdua = tujuandua.getSheetByName("lalala");
+  sheetdua.getRange(2,1,maxrow,2).setValues(range);
 }
